29d5fce186fa0861b34b41f05192f0a83b704089
David Yearwood <dwhywood@gmail.com>
2020-07-01 11:17:42 -0400

    chore: configure webpack for dev environment

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  // the file where we begin the bundle process
+  entry: './index.js',
+
+  // where we are going to emit the new bundled file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // has to be an absolute path
+    filename: 'modal.js',
+  },
+
+  // Loaders
+  module: {
+    // Takes our ES6 javascript and make it compatible w/ browsers old and new
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
